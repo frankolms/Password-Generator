b1b25b3495ecb891b2247fb417d6acb374f1aad5
frankolms <frankolms@gmail.com>
2023-02-13 22:33:50 -0500

    final product

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Password-Generator
+
+This is a password generator that takes in inputs from the user to output a password that meets those parameters.
+
+When the user presses the Generate Password button, the user is presented with a prompt alert that asks them to input a number value between 8 and 128 that will determine the length of the password. The user is then presented with numerous confirm alerts that ask if they would like to include uppercase letters, lowercase letters, numbers, and special characters in their password. The user must answer yes to at least one of these prompts. Once the user correctly inputs a length for the password and answers yes to at least one of the confirm alerts, a password is generated and then displayed in the box with placeholder text that says Your Secure Password.
+
+If the user inputs a number outside of the 8 - 128 character requirement, or the user says no to all of the confirm prompts, it will ask the user to try again.
+
+![Alt text](<./assets/Screenshot%20(32).png>)
--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,265 @@
 // Assignment code here
-var howLong = prompt(
-  "How long would you like the password to be? Please choose 8 - 128 characters"
-);
+//Arrays to store the characters used to generate the password
+var upperCaseLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
-var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-
-var numbers = "0123456789";
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var specCharacters = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+[
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "(",
+  ")",
+  "*",
+  "+",
+  "=",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "}",
+  "|",
+  "~",
+];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-var generatePassword = function (num) {
-  var upperCase = confirm("Would you like to include Upper Case letters?");
-  var lowerCase = confirm("Would you like to include Lower Case letters?");
-  var specialChar = confirm("Would you like to include Special Characters?");
-  var num = confirm("Would you like to include numbers?");
-
-  if (
-    upperCase === false &&
-    lowerCase === false &&
-    specialChar === false &&
-    num === false
-  ) {
-    alert(
-      "You must choose at least one parameter. Please refresh and try again."
-    );
-  }
-
-  if (howLong < 8 || howLong > 128) {
+//function to generate the password
+var generatePassword = function () {
+  var howLong = prompt(
+    "How long would you like the password to be? Please choose 8 - 128 characters"
+  );
+  //if statement to make sure the proper length parameter is input
+  if (howLong < 8 || howLong > 128 || isNaN(howLong)) {
     alert(
       "Your input does not match the character length criteria. Please refresh and try again."
     );
+  } else {
+    var upperCase = confirm("Would you like to include Upper Case letters?");
+    var lowerCase = confirm("Would you like to include Lower Case letters?");
+    var specialChar = confirm("Would you like to include Special Characters?");
+    var num = confirm("Would you like to include numbers?");
+  }
+  //object to store users input values
+  var usersChoices = {
+    howLong,
+    upperCase,
+    lowerCase,
+    specialChar,
+    num,
+  };
+
+  console.log(usersChoices);
+  //if statements to account for all possibilities of password parameter choices by the user
+  if (
+    usersChoices.upperCase === false &&
+    usersChoices.lowerCase === false &&
+    usersChoices.specialChar === false &&
+    usersChoices.num === false
+  ) {
+    alert("You must choose at least one parameter. Please try again.");
   }
 
-  var result = " ";
-  var finalPassword = upperCaseLetters.length;
-  for (var i = 0; i < howLong; i++) {
-    result += upperCaseLetters.charAt(
-      Math.floor(Math.random() * finalPassword)
+  var userChoice = [];
+
+  if (
+    usersChoices.upperCase === true &&
+    usersChoices.lowerCase === false &&
+    usersChoices.specialChar === false &&
+    usersChoices.num === false
+  ) {
+    userChoice.push(...upperCaseLetters);
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === true &&
+    usersChoices.lowerCase === true &&
+    usersChoices.specialChar === false &&
+    usersChoices.num === false
+  ) {
+    userChoice.push(...upperCaseLetters, ...lowerCaseLetters);
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === true &&
+    usersChoices.lowerCase === true &&
+    usersChoices.specialChar === true &&
+    usersChoices.num === false
+  ) {
+    userChoice.push(
+      ...upperCaseLetters,
+      ...lowerCaseLetters,
+      ...specCharacters
     );
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === true &&
+    usersChoices.lowerCase === true &&
+    usersChoices.specialChar === true &&
+    usersChoices.num === true
+  ) {
+    userChoice.push(
+      ...upperCaseLetters,
+      ...lowerCaseLetters,
+      ...specCharacters,
+      ...numbers
+    );
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === false &&
+    usersChoices.lowerCase === true &&
+    usersChoices.specialChar === false &&
+    usersChoices.num === false
+  ) {
+    userChoice.push(...lowerCaseLetters);
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === false &&
+    usersChoices.lowerCase === true &&
+    usersChoices.specialChar === true &&
+    usersChoices.num === false
+  ) {
+    userChoice.push(...lowerCaseLetters, ...specCharacters);
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === false &&
+    usersChoices.lowerCase === true &&
+    usersChoices.specialChar === true &&
+    usersChoices.num === true
+  ) {
+    userChoice.push(...lowerCaseLetters, ...specCharacters, ...numbers);
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === true &&
+    usersChoices.lowerCase === false &&
+    usersChoices.specialChar === true &&
+    usersChoices.num === false
+  ) {
+    userChoice.push(...upperCaseLetters, ...specCharacters);
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === true &&
+    usersChoices.lowerCase === false &&
+    usersChoices.specialChar === false &&
+    usersChoices.num === true
+  ) {
+    userChoice.push(...upperCaseLetters, ...numbers);
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === false &&
+    usersChoices.lowerCase === true &&
+    usersChoices.specialChar === false &&
+    usersChoices.num === true
+  ) {
+    userChoice.push(...lowerCaseLetters, ...numbers);
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === false &&
+    usersChoices.lowerCase === false &&
+    usersChoices.specialChar === true &&
+    usersChoices.num === true
+  ) {
+    userChoice.push(...specCharacters, ...numbers);
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === false &&
+    usersChoices.lowerCase === false &&
+    usersChoices.specialChar === false &&
+    usersChoices.num === true
+  ) {
+    userChoice.push(...numbers);
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === false &&
+    usersChoices.lowerCase === false &&
+    usersChoices.specialChar === true &&
+    usersChoices.num === false
+  ) {
+    userChoice.push(...specCharacters);
+    console.log(userChoice);
+  } else if (
+    usersChoices.upperCase === true &&
+    usersChoices.lowerCase === false &&
+    usersChoices.specialChar === true &&
+    usersChoices.num === true
+  ) {
+    userChoice.push(...upperCaseLetters, ...specCharacters, ...numbers);
+    console.log(userChoice);
+  }
+  //loop to generate the password based on the users inputs
+  var result = " ";
+  var finalPassword = userChoice.length;
+  for (var i = 0; i < usersChoices.howLong; i++) {
+    result += userChoice[Math.floor(Math.random() * finalPassword)];
   }
 
   return result;
@@ -58,5 +275,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-console.log(generatePassword(howLong));
